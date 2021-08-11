34d9b4d10e6998e55e054c0eb27a62b8640ed96d
avvy123 <ak9162834595@gmail.com>
2021-08-11 14:21:14 +0530

    add delete feature

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const taskContainer = document.querySelector(".task__container");
 
-const globalStore = [];
+let globalStore = [];
 
 const generateNewCard = (taskData) => `
 <div class="col-md-6 col-lg-4" id=${taskData.id}>
@@ -9,8 +9,8 @@ const generateNewCard = (taskData) => `
 <button type="button" class="btn btn-outline-success">
 <i class="fas fa-pencil-alt"></i>
 </button>
-        <button type="button" class="btn btn-outline-danger">
-        <i class="fas fa-trash-alt"></i>
+        <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this, arguments)">
+        <i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this, arguments)"></i>
         </button>
         </div>
         <img src=${taskData.imageUrl} class="card-img-top" alt="...">
@@ -35,17 +35,18 @@ const loadInitialCardData = () => {
 
     const {cards} = JSON.parse(getCardData);
 
-    // loop over those arry of task object to create HTML Card, inject it to DOM
+    // loop over those arry of task object to create HTML Card
 
     cards.map((cardObject) => {
+
+        // inject to DOM
         taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
 
         // update our global store
         globalStore.push(cardObject);
-    });
+    })
+};
 
-
-}
 const saveChanges = () => {
             
 const taskData = {
@@ -61,4 +62,23 @@ const taskData = {
     globalStore.push(taskData);
 
     localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+};
+
+const deleteCard = (event) => {
+    event = window.event;
+    // id
+    const targetID = event.target.id;
+    const tagname = event.target.tagName;
+
+    // match the id of the element with the id inside the globalStore
+    // if match found remove 
+    globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID); 
+    localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+
+    // contact parent
+    if(tagname === "BUTTON"){
+        return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+    } else{
+        return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+    }
 };
